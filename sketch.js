ab8d12281fee2e8d0e0df1fa015f7dcff4e0a5a5
Tori Gray <tgray24@lsu.edum>
2025-09-10 11:59:34 -0500

    Finished02

--- sketch.js
+++ sketch.js
@@ -1,7 +1,85 @@
 function setup() {
-  createCanvas(400, 269);
+  createCanvas(1900, 642);
 }
 
 function draw() {
+  background('#D9AE09');
 
+  fill(0);
+  ellipse(640, 245, 315, 80);
+
+  fill(0);
+  ellipse(440, 350, 120, 80);
+
+  fill(0);
+  ellipse(180, 340, 130, 100);
+
+  fill(0);
+  ellipse(785, 335, 110, 100);
+
+  fill(0);
+  ellipse(885, 190, 50, 50);
+
+  fill(0);
+  ellipse(500, 115, 130, 190);
+
+  fill(0);
+  ellipse(330, 30, 140, 210);
+
+  fill(0);
+  ellipse(1110, 335, 250, 280);
+
+  fill(0);
+  ellipse(1300, 265, 260, 360);
+
+  fill(0);
+  ellipse(785, 90, 120, 160);
+
+  fill(0);
+  ellipse(180, 170, 240, 100);
+
+  fill('#D9AE09');
+  noStroke();
+  ellipse(180, 170, 80, 130);
+  
+  fill('#D9AE09');
+  noStroke();
+  ellipse(640, 255, 90, 90);
+
+  fill(0);
+  ellipse(1500, 280, 90, 100);
+
+  fill(0);
+  ellipse(1660, 220, 120, 180);
+
+  fill(0);
+  ellipse(1830, 150, 110, 200);
+
+  fill(0);
+  ellipse(1490, 60, 140, 200);
+  
+  stroke(0);
+  strokeWeight(100);
+  line(1370, 0, 1500, 100);
+
+  stroke(0);
+  strokeWeight(50);
+  line(1670, 0, 1700, 100);
+
+
+  fill('#690002');
+  noStroke();
+  rect(0, 348, 1900, 350);
+
+  stroke(0);
+  strokeWeight(15);
+  line(0, 348, 1900, 348);
+
+  stroke(0);
+  strokeWeight(18);
+  line(0, 442, 1900, 442);
+
+  stroke(0);
+  strokeWeight(18);
+  line(0, 524, 1900, 524);
 }
